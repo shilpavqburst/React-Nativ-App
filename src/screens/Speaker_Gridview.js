832ff92fd4bc6b76f295aa8d6aea2e40a3169741
shilpavqburst <73292166+shilpavqburst@users.noreply.github.com>
2021-02-24 10:59:58 +0530

    Update Speaker_Gridview.js

--- src/screens/Speaker_Gridview.js
+++ src/screens/Speaker_Gridview.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
+import DropDownPicker from 'react-native-dropdown-picker';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {
   SafeAreaView,
   StyleSheet,
@@ -67,83 +69,211 @@ const arr = [
   },
 ];
 
-const App = () => {
+const App = ({navigation}) => {
+  const [country, setCountry] = useState('ernakulam');
   return (
     <SafeAreaView style={{flex: 1}}>
-      <StatusBar barStyle="dark-content" />
       <View style={styles.head}>
+        <TouchableNativeFeedback
+          onPress={() => {
+            navigation.goBack();
+          }}>
+          <Image
+            style={{
+              height: 18,
+              width: 10,
+              marginLeft: '3%',
+              marginTop: 33,
+              marginBottom: 12.97,
+            }}
+            source={require('../assets/images/titlebar_back_arrow.png')}></Image>
+        </TouchableNativeFeedback>
         <Text
           style={{
             fontFamily: 'bariol_regular-webfont',
             fontSize: 30,
             color: 'rgba(255,121,86,1)',
-            //margin: 20,
+            marginTop: 33,
+            marginBottom: 12.97,
           }}>
-          electronics
+          speakers
         </Text>
+        <View>
+          <Image
+            style={{
+              height: 19,
+              width: 19,
+              marginRight: '3%',
+              marginTop: 33,
+              marginBottom: 12.97,
+            }}
+            source={require('../assets/images/search_lens.png')}></Image>
+        </View>
       </View>
-
       <ScrollView
         style={{backgroundColor: '#e8e8e8'}}
         contentContainerStyle={{
-          flexDirection: 'row',
-          flexWrap: 'wrap',
+          flexDirection: 'column',
+          //flexWrap: 'wrap',
         }}>
-        {arr.map((itm, indx) => (
-          <View style={styles.box}>
-            <View>
-              <ImageBackground
-                source={itm.img}
+        <View
+          style={{
+            paddingVertical: 5,
+            backgroundColor: 'rgba(248,248,248,1)',
+            // flexDirection: 'row',
+            justifyContent: 'center',
+            zIndex: 10,
+            borderBottomWidth: 1,
+            borderColor: 'rgba(231,231,231,1)',
+            height: 50,
+          }}>
+          <DropDownPicker
+            items={[
+              {
+                label: 'ernakulam',
+                value: 'ernakulam',
+                //hidden: true,
+              },
+              {
+                label: 'trivandrum',
+                value: 'trivandrum',
+              },
+              {
+                label: 'thrissur',
+                value: 'thrissur',
+              },
+            ]}
+            defaultValue={country}
+            style={{
+              backgroundColor: 'rgba(248,248,248,1)',
+              borderWidth: 0,
+              width: 120,
+            }}
+            containerStyle={{zIndex: 20, position: 'absolute'}}
+            itemStyle={{
+              justifyContent: 'flex-start',
+            }}
+            globalTextStyle={{
+              fontSize: 18,
+              fontFamily: 'bariol_regular-webfont',
+              color: 'rgba(162,134,128,1)',
+              //left: -15,
+            }}
+            selectedLabelStyle={{marginLeft: -1}}
+            dropDownStyle={{
+              backgroundColor: '#fafafa',
+            }}
+            onOpen={() => {}}
+            onClose={() => {}}
+            customArrowDown={(size) => (
+              <Icon name="caret-down" size={15} style={{top: 2}} />
+            )}
+            onChangeItem={(item) => setCountry(item.value)}
+          />
+          <View style={{flexDirection: 'row', alignSelf: 'flex-end'}}>
+            <Image
+              style={{
+                height: 21,
+                width: 21,
+                //alignSelf: 'flex-end',
+                marginRight: 20,
+                marginTop: 15,
+                marginBottom: 15,
+              }}
+              source={require('../assets/images/filter_icn.png')}
+            />
+            <TouchableNativeFeedback
+              onPress={() => {
+                navigation.goBack();
+              }}>
+              <Image
                 style={{
-                  //marginBottom: 5,
-                  // height: 185,
-                  width: '100%',
-                }}>
-                <View
-                  style={{
-                    width: '100%',
-                    paddingLeft: 45,
-                    backgroundColor: '#1d1d1d',
-                    marginTop: 145,
-                    //bottom: 20,
-                    //marginLeft: 5,
-                    height: 40,
-                  }}>
-                  <Text
-                    style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'bariol_regular-webfont',
-                      fontSize: 14,
-                      marginLeft: -30,
-                      top: 5,
-                    }}>
-                    {itm.title}
-                  </Text>
-                  <Text
-                    style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'bariol_regular-webfont',
-                      fontSize: 12,
-                      marginLeft: -30,
-                      top: 5,
-                    }}>
-                    {itm.basetext}
-                  </Text>
-                  <Text
-                    style={{
-                      color: '#FF7F50',
-                      fontFamily: 'bariol_regular-webfont',
-                      fontSize: 14,
-                      marginLeft: 90,
-                      bottom: 20,
-                    }}>
-                    {itm.price}
-                  </Text>
-                </View>
-              </ImageBackground>
-            </View>
+                  height: 20,
+                  width: 20,
+                  //alignSelf: 'flex-end',
+                  // alignItems: 'center',
+                  //justifyContent: 'center',
+                  marginRight: 10,
+                  marginTop: 15,
+                  marginBottom: 15,
+                }}
+                source={require('../assets/images/listview_icn.png')}
+              />
+            </TouchableNativeFeedback>
           </View>
-        ))}
+        </View>
+        <ImageBackground
+          style={{height: '100%', width: '100%'}}
+          source={require('../assets/images/bg.png')}>
+          <View
+            style={{
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              justifyContent: 'space-evenly',
+              top: 15,
+            }}>
+            {arr.map((itm, indx) => (
+              <View style={styles.box}>
+                <View>
+                  <ImageBackground
+                    source={itm.img}
+                    style={
+                      {
+                        //marginBottom: 5,
+                        // height: 185,
+                        // width: '100%',
+                      }
+                    }>
+                    <View
+                      style={{
+                        //width: '100%',
+                        paddingLeft: 45,
+                        backgroundColor: '#1d1d1d',
+                        marginTop: 145,
+                        //bottom: 20,
+                        //marginLeft: 5,
+                        height: 40,
+                        justifyContent: 'space-between',
+                      }}>
+                      <Text
+                        style={{
+                          color: '#FFFFFF',
+                          fontFamily: 'bariol_regular-webfont',
+                          fontSize: 14,
+                          marginLeft: -40,
+                          top: 6,
+                        }}>
+                        {itm.title}
+                      </Text>
+                      <Text
+                        style={{
+                          color: '#FFFFFF',
+                          fontFamily: 'bariol_regular-webfont',
+                          fontSize: 12,
+                          marginLeft: -40,
+                          top: 7,
+                        }}>
+                        {itm.basetext}
+                      </Text>
+
+                      <Text
+                        style={{
+                          color: '#FF7F50',
+                          fontFamily: 'bariol_regular-webfont',
+                          fontSize: 14,
+                          marginLeft: 75,
+                          bottom: 15,
+                          right: 5,
+                        }}>
+                        {itm.price}
+                      </Text>
+                    </View>
+                  </ImageBackground>
+                </View>
+              </View>
+            ))}
+          </View>
+        </ImageBackground>
       </ScrollView>
     </SafeAreaView>
   );
@@ -158,18 +288,22 @@ const styles = StyleSheet.create({
   },
   head: {
     backgroundColor: '#FFFFFF',
-    height: 70,
+    height: 64,
     width: '100%',
     alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(232,232,232,1)',
   },
   box: {
-    width: '47%',
-    height: 185,
+    width: 174,
+    height: 174,
     alignItems: 'center',
-    margin: 4,
-    backgroundColor: '#FFFFFF',
+    //margin: 9,
+    //backgroundColor: '#FFFFFF',
     justifyContent: 'center',
+    marginBottom: 20,
   },
   Image: {
     width: 50,
