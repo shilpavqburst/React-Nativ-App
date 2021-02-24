3a428fa7734a5355ea4f244fc2f4f2fe6128232b
shilpavqburst <73292166+shilpavqburst@users.noreply.github.com>
2021-02-24 10:54:33 +0530

    Update Location.js

--- src/screens/Location.js
+++ src/screens/Location.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-
 import {
   SafeAreaView,
   StyleSheet,
@@ -9,11 +8,11 @@ import {
   TextInput,
   StatusBar,
   Image,
-  ImageBackground,
+  TouchableNativeFeedback,
 } from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 
-const App = () => {
+const App = ({navigation}) => {
   const [region, setRegion] = useState({
     latitude: 37.78825,
     longitude: -122.4324,
@@ -29,6 +28,36 @@ const App = () => {
   };
   return (
     <SafeAreaView style={{flex: 1}}>
+      <View style={styles.head}>
+        <TouchableNativeFeedback
+          onPress={() => {
+            navigation.goBack();
+          }}>
+          <Image
+            style={{
+              height: 18.03,
+              width: 9.7,
+              marginLeft: 10,
+              marginTop: 33,
+              marginBottom: 12.97,
+              // alignSelf: 'flex-start',
+            }}
+            source={require('../assets/images/titlebar_back_arrow.png')}></Image>
+        </TouchableNativeFeedback>
+        <Text
+          style={{
+            fontFamily: 'bariol_regular-webfont',
+            fontSize: 30,
+            color: 'rgba(255,121,86,1)',
+            //marginLeft: 121.3,
+            marginBottom: 12.97,
+            marginTop: 33,
+            alignSelf: 'center',
+          }}>
+          location
+        </Text>
+        <View />
+      </View>
       <MapView style={{flex: 1}} initialRegion={region}>
         <Marker
           coordinate={{
@@ -44,13 +73,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-
-  slideContainer: {
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    height: 300,
+  head: {
+    backgroundColor: '#FFFFFF',
+    height: 64,
     width: '100%',
-    backgroundColor: 'red',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(232,232,232,1)',
   },
 
   small: {
